48168a3ae60d12a2756c556ea349c7259968180b
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2024-05-31 13:17:02 +0200

    implemented trng with WSS

--- main/websocket_example.c
+++ main/websocket_example.c
@@ -1,18 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Unlicense OR CC0-1.0
- */
-/* ESP HTTP Client Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
-
 #include <stdio.h>
 #include "esp_wifi.h"
 #include "esp_system.h"
@@ -30,6 +15,8 @@
 #include "esp_event.h"
 #include <cJSON.h>
 
+#include "esp_random.h"
+
 #define NO_DATA_TIMEOUT_SEC 5
 
 static const char *TAG = "websocket";
@@ -39,7 +26,8 @@ static SemaphoreHandle_t shutdown_sema;
 
 static void log_error_if_nonzero(const char *message, int error_code)
 {
-    if (error_code != 0) {
+    if (error_code != 0)
+    {
         ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
     }
 }
@@ -54,12 +42,16 @@ static void shutdown_signaler(TimerHandle_t xTimer)
 static void get_string(char *line, size_t size)
 {
     int count = 0;
-    while (count < size) {
+    while (count < size)
+    {
         int c = fgetc(stdin);
-        if (c == '\n') {
+        if (c == '\n')
+        {
             line[count] = '\0';
             break;
-        } else if (c > 0 && c < 127) {
+        }
+        else if (c > 0 && c < 127)
+        {
             line[count] = c;
             ++count;
         }
@@ -72,32 +64,39 @@ static void get_string(char *line, size_t size)
 static void websocket_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
     esp_websocket_event_data_t *data = (esp_websocket_event_data_t *)event_data;
-    switch (event_id) {
+    switch (event_id)
+    {
     case WEBSOCKET_EVENT_CONNECTED:
         ESP_LOGI(TAG, "WEBSOCKET_EVENT_CONNECTED");
         break;
     case WEBSOCKET_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "WEBSOCKET_EVENT_DISCONNECTED");
-        log_error_if_nonzero("HTTP status code",  data->error_handle.esp_ws_handshake_status_code);
-        if (data->error_handle.error_type == WEBSOCKET_ERROR_TYPE_TCP_TRANSPORT) {
+        log_error_if_nonzero("HTTP status code", data->error_handle.esp_ws_handshake_status_code);
+        if (data->error_handle.error_type == WEBSOCKET_ERROR_TYPE_TCP_TRANSPORT)
+        {
             log_error_if_nonzero("reported from esp-tls", data->error_handle.esp_tls_last_esp_err);
             log_error_if_nonzero("reported from tls stack", data->error_handle.esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno",  data->error_handle.esp_transport_sock_errno);
+            log_error_if_nonzero("captured as transport's socket errno", data->error_handle.esp_transport_sock_errno);
         }
         break;
     case WEBSOCKET_EVENT_DATA:
         ESP_LOGI(TAG, "WEBSOCKET_EVENT_DATA");
         ESP_LOGI(TAG, "Received opcode=%d", data->op_code);
-        if (data->op_code == 0x08 && data->data_len == 2) {
+        if (data->op_code == 0x08 && data->data_len == 2)
+        {
             ESP_LOGW(TAG, "Received closed message with code=%d", 256 * data->data_ptr[0] + data->data_ptr[1]);
-        } else {
+        }
+        else
+        {
             ESP_LOGW(TAG, "Received=%.*s\n\n", data->data_len, (char *)data->data_ptr);
         }
 
         // If received data contains json structure it succeed to parse
         cJSON *root = cJSON_Parse(data->data_ptr);
-        if (root) {
-            for (int i = 0 ; i < cJSON_GetArraySize(root) ; i++) {
+        if (root)
+        {
+            for (int i = 0; i < cJSON_GetArraySize(root); i++)
+            {
                 cJSON *elem = cJSON_GetArrayItem(root, i);
                 cJSON *id = cJSON_GetObjectItem(elem, "id");
                 cJSON *name = cJSON_GetObjectItem(elem, "name");
@@ -112,11 +111,12 @@ static void websocket_event_handler(void *handler_args, esp_event_base_t base, i
         break;
     case WEBSOCKET_EVENT_ERROR:
         ESP_LOGI(TAG, "WEBSOCKET_EVENT_ERROR");
-        log_error_if_nonzero("HTTP status code",  data->error_handle.esp_ws_handshake_status_code);
-        if (data->error_handle.error_type == WEBSOCKET_ERROR_TYPE_TCP_TRANSPORT) {
+        log_error_if_nonzero("HTTP status code", data->error_handle.esp_ws_handshake_status_code);
+        if (data->error_handle.error_type == WEBSOCKET_ERROR_TYPE_TCP_TRANSPORT)
+        {
             log_error_if_nonzero("reported from esp-tls", data->error_handle.esp_tls_last_esp_err);
             log_error_if_nonzero("reported from tls stack", data->error_handle.esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno",  data->error_handle.esp_transport_sock_errno);
+            log_error_if_nonzero("captured as transport's socket errno", data->error_handle.esp_transport_sock_errno);
         }
         break;
     }
@@ -145,11 +145,11 @@ static void websocket_app_start(void)
 
 #if CONFIG_WS_OVER_TLS_MUTUAL_AUTH
     /* Configuring client certificates for mutual authentification */
-    extern const char cacert_start[] asm("_binary_ca_cert_pem_start"); // CA certificate
+    extern const char cacert_start[] asm("_binary_ca_cert_pem_start");   // CA certificate
     extern const char cert_start[] asm("_binary_client_cert_pem_start"); // Client certificate
-    extern const char cert_end[]   asm("_binary_client_cert_pem_end");
+    extern const char cert_end[] asm("_binary_client_cert_pem_end");
     extern const char key_start[] asm("_binary_client_key_pem_start"); // Client private key
-    extern const char key_end[]   asm("_binary_client_key_pem_end");
+    extern const char key_end[] asm("_binary_client_key_pem_end");
 
     websocket_cfg.cert_pem = cacert_start;
     websocket_cfg.client_cert = cert_start;
@@ -172,24 +172,17 @@ static void websocket_app_start(void)
 
     esp_websocket_client_start(client);
     xTimerStart(shutdown_signal_timer, portMAX_DELAY);
-    char data[32];
-    int i = 0;
-    while (i < 5) {
-        if (esp_websocket_client_is_connected(client)) {
-            int len = sprintf(data, "hello %04d", i++);
-            ESP_LOGI(TAG, "Sending %s", data);
-            esp_websocket_client_send_text(client, data, len, portMAX_DELAY);
-        }
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
 
-    ESP_LOGI(TAG, "Sending fragmented message");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    memset(data, 'a', sizeof(data));
-    esp_websocket_client_send_text_partial(client, data, sizeof(data), portMAX_DELAY);
-    memset(data, 'b', sizeof(data));
-    esp_websocket_client_send_cont_msg(client, data, sizeof(data), portMAX_DELAY);
-    esp_websocket_client_send_fin(client, portMAX_DELAY);
+    ESP_LOGI(TAG, "Starting transmission...");
+
+    while (true)
+    {
+        if (esp_websocket_client_is_connected(client))
+        {
+            uint32_t random_number = esp_random();
+            esp_websocket_client_send_bin(client, (char *)&random_number, sizeof(random_number), portMAX_DELAY);
+        }
+    }
 
     xSemaphoreTake(shutdown_sema, portMAX_DELAY);
     esp_websocket_client_close(client, portMAX_DELAY);
